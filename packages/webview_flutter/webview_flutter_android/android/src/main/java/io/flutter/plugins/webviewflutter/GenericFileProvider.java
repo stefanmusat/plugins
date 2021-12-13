d6e40b5a8d256c04b3bea47f48b9b2bc3de4e624
stefanmusat <86401773+stefanmusat@users.noreply.github.com>
2021-12-13 11:38:01 +0200

    Update GenericFileProvider.java

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/GenericFileProvider.java
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/GenericFileProvider.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.flutter.plugins.webviewflutter;
 
 import androidx.core.content.FileProvider;
@@ -6,12 +5,4 @@ import androidx.core.content.FileProvider;
 public class GenericFileProvider extends FileProvider {
     
 }
-=======
-package io.flutter.plugins.webviewflutter;
 
-import androidx.core.content.FileProvider;
-
-public class GenericFileProvider extends FileProvider {
-    
-}
->>>>>>> 9850a5a7c6cdf2ab63d60f40ea4677dddacf5e17
