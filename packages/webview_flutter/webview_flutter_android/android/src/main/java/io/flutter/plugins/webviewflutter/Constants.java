e1ca1635424f4b95f56a284e60cbe5163ac284fc
stefanmusat <86401773+stefanmusat@users.noreply.github.com>
2021-12-13 12:33:20 +0200

    Update Constants.java

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/Constants.java
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/Constants.java
@@ -1,17 +1,14 @@
 package io.flutter.plugins.webviewflutter;
 
 public class Constants {
-    static final String ACTION_REQUEST_CAMERA_PERMISSION_FINISHED =
-            "action_request_camera_permission_denied";
-    static final String ACTION_FILE_CHOOSER_FINISHED = "action_file_chooser_completed";
+    public static final String ACTION_PERMISSIONS_GRANTED = "action_permission_granted";
+    public static final String ACTION_PERMISSIONS_DENIED = "action_permission_denied";
 
-    static final String EXTRA_TITLE = "extra_title";
-    static final String EXTRA_ACCEPT_TYPES = "extra_types";
-    static final String EXTRA_SHOW_VIDEO_OPTION = "extra_show_video_option";
-    static final String EXTRA_SHOW_IMAGE_OPTION = "extra_show_image_option";
-    static final String EXTRA_FILE_URIS = "extra_file_uris";
-    static final String EXTRA_ALLOW_MULTIPLE_FILES = "extra_allow_multiple_files";
+    public static final String ACTION_FILE_CHOOSER_FINISHED = "action_file_chooser_completed";
 
+    public static final String EXTRA_SHOW_CAMERA_OPTION = "extra_show_camera_option";
+    public static final String EXTRA_IMAGE_URI = "extra_image_uri";
 
-    static final String WEBVIEW_STORAGE_DIRECTORY = "images";
+    public static final String WEBVIEW_CAMERA_IMAGE_DIRECTORY = "camera_images";
+    public static final String WEBVIEW_CAMERA_IMAGE_FILE_NAME = "image.jpg";
 }
