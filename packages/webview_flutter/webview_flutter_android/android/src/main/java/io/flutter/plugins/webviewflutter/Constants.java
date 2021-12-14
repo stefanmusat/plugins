e5a0ba65d5fa425e18ac69c6b7711e6d59608a10
stefanmusat <86401773+stefanmusat@users.noreply.github.com>
2021-12-14 10:28:28 +0200

    Update Constants.java

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/Constants.java
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/Constants.java
@@ -11,4 +11,18 @@ public class Constants {
 
     public static final String WEBVIEW_CAMERA_IMAGE_DIRECTORY = "camera_images";
     public static final String WEBVIEW_CAMERA_IMAGE_FILE_NAME = "image.jpg";
+    
+       static final String ACTION_REQUEST_CAMERA_PERMISSION_FINISHED =
+                "action_request_camera_permission_denied";
+
+    
+        static final String EXTRA_TITLE = "extra_title";
+        static final String EXTRA_ACCEPT_TYPES = "extra_types";
+        static final String EXTRA_SHOW_VIDEO_OPTION = "extra_show_video_option";
+        static final String EXTRA_SHOW_IMAGE_OPTION = "extra_show_image_option";
+        static final String EXTRA_FILE_URIS = "extra_file_uris";
+        static final String EXTRA_ALLOW_MULTIPLE_FILES = "extra_allow_multiple_files";
+    
+    
+        static final String WEBVIEW_STORAGE_DIRECTORY = "images";
 }
